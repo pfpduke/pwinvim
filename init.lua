2f904d19bf185c2c3bbc4823fff335680e37947a
Ian Sterner <pfp_duke@yahoo.com>
2025-04-26 22:31:07 -0500

    Fully working Powershell IDE

--- init.lua
+++ init.lua
@@ -2,6 +2,12 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Custom options
+vim.opt.tabstop = 4        -- A tab character looks like 4 spaces
+vim.opt.shiftwidth = 4     -- Indents (like >> or <<) are 4 spaces
+vim.opt.expandtab = true   -- Always insert spaces instead of real tab characters
+
+
 -- Lazy.nvim Bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -18,7 +24,8 @@ require("lazy").setup({
   { import = "plugins" },
 })
 
-vim.cmd("colorscheme tokyonight")
+--vim.cmd("colorscheme tokyonight")
+--vim.cmd("colorscheme tokyonight")
 
 
 
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,13 @@
 return {
-    {
-      "folke/tokyonight.nvim",
-      lazy = false,
-      priority = 1000,
-    }
-  }
-  
+  "folke/tokyonight.nvim",
+  priority = 1000,
+  config = function()
+    require("tokyonight").setup({
+      style = "night",
+      --style = "storm",
+      --style = "moon",
+      --style = "day",
+    })
+    vim.cmd("colorscheme tokyonight")
+  end,
+}
